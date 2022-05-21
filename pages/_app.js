2b1577d62cf04f052b75ee0388b88d1abcf571d8
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-05-21 23:02:53 +0530

    add seo tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
+import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 import '../styles/globals.css'
 import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
-  return <ThemeProvider theme={theme}>
-    <Component {...pageProps} />
-  </ThemeProvider>
+  return <>
+    <Head>
+      <title>Shortly</title>
+
+      <link href='/favicon.ico' rel='icon' />
+    </Head>
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
 }
 
 export default MyApp
