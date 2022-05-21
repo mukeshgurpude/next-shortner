e08d5af735b3b3c246da22749aacc1a49773cf9b
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-05-21 22:49:43 +0530

    uconfigure theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
+import { ThemeProvider } from 'styled-components'
 import '../styles/globals.css'
+import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </ThemeProvider>
 }
 
 export default MyApp
---
+++ styles/theme.js
@@ -0,0 +1,20 @@
+/**@type { import('styled-system').Theme } */
+const theme = {
+  breakpoints: [375, 1440],
+  colors: {
+    primary: {
+      cyan: '#2acfcf',
+      violet: '#3b3054'
+    },
+    secondary: '#f46262',
+    neutral: {
+      gray: '#bfbfbf',
+      violet: '#9e9aa7',
+      blue: '#35323e',
+      dark_violet: '#232127'
+    }
+  },
+  fontWeights: [500, 700]
+}
+
+export default theme
